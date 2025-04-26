7eedc844f334110be3edfe31d181f101e58cb160
annkafake <lainfhj@proton.me>
2025-04-25 21:51:19 -0500

    update entity ReportePubCli

--- src/entidades/ReportePubCli.java
+++ src/entidades/ReportePubCli.java
@@ -3,19 +3,41 @@ package entidades;
 public class ReportePubCli {
 	// reporte dinamico para mostrar las publicidades 
 	// asignadas a clientes, y busqueda por cliente
+	private int id_cli;
+	private String nombre_cli;
 	private int id_pub;
-	private String nombre;
 	private String desc_pub;
+	private String tipo_pub;
+	private String fecha;
 	
 	public ReportePubCli() {
 		super();
 	}
 
-	public ReportePubCli(int id_pub ,String nombre, String desc_pub) {
+	public ReportePubCli(int id_cli, String nombre_cli, int id_pub, String desc_pub, String tipo_pub, String fecha) {
 		super();
+		this.id_cli = id_cli;
+		this.nombre_cli = nombre_cli;
 		this.id_pub = id_pub;
-		this.nombre = nombre;
 		this.desc_pub = desc_pub;
+		this.tipo_pub = tipo_pub;
+		this.fecha = fecha;
+	}
+
+	public int getId_cli() {
+		return id_cli;
+	}
+
+	public void setId_cli(int id_cli) {
+		this.id_cli = id_cli;
+	}
+
+	public String getNombre_cli() {
+		return nombre_cli;
+	}
+
+	public void setNombre_cli(String nombre_cli) {
+		this.nombre_cli = nombre_cli;
 	}
 
 	public int getId_pub() {
@@ -26,14 +48,6 @@ public class ReportePubCli {
 		this.id_pub = id_pub;
 	}
 
-	public String getNombre() {
-		return nombre;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
 	public String getDesc_pub() {
 		return desc_pub;
 	}
@@ -41,6 +55,21 @@ public class ReportePubCli {
 	public void setDesc_pub(String desc_pub) {
 		this.desc_pub = desc_pub;
 	}
-	
+
+	public String getTipo_pub() {
+		return tipo_pub;
+	}
+
+	public void setTipo_pub(String tipo_pub) {
+		this.tipo_pub = tipo_pub;
+	}
+
+	public String getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(String fecha) {
+		this.fecha = fecha;
+	}
 	
 }
