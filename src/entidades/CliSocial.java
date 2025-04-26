9f51229a0ba513dcc923ecc780d1a7e5ddd23401
annkafake <lainfhj@proton.me>
2025-04-25 20:02:05 -0500

    add entity CliSocial

---
+++ src/entidades/CliSocial.java
@@ -0,0 +1,54 @@
+package entidades;
+
+public class CliSocial {
+	private int id_cli_soc;
+	private String user;
+	private String proveedor;
+	private int id_cli;
+	
+	public CliSocial() {
+		super();
+	}
+
+	public CliSocial(int id_cli_soc, String user, String proveedor, int id_cli) {
+		super();
+		this.id_cli_soc = id_cli_soc;
+		this.user = user;
+		this.proveedor = proveedor;
+		this.id_cli = id_cli;
+	}
+
+	public int getId_cli_soc() {
+		return id_cli_soc;
+	}
+
+	public void setId_cli_soc(int id_cli_soc) {
+		this.id_cli_soc = id_cli_soc;
+	}
+
+	public String getUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+	public String getProveedor() {
+		return proveedor;
+	}
+
+	public void setProveedor(String proveedor) {
+		this.proveedor = proveedor;
+	}
+
+	public int getId_cli() {
+		return id_cli;
+	}
+
+	public void setId_cli(int id_cli) {
+		this.id_cli = id_cli;
+	}
+	
+	
+}
