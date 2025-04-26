3480b74c4af5a1ef556b191cfb3712d6bd959b2b
annkafake <lainfhj@proton.me>
2025-04-25 19:59:43 -0500

    add entity CliCorreo

---
+++ src/entidades/CliCorreo.java
@@ -0,0 +1,54 @@
+package entidades;
+
+public class CliCorreo {
+	private int id_cli_cor;
+	private String correo;
+	private String proveedor;
+	private int id_cli;
+	
+	public CliCorreo() {
+		super();
+	}
+
+	public CliCorreo(int id_cli_cor, String correo, String proveedor, int id_cli) {
+		super();
+		this.id_cli_cor = id_cli_cor;
+		this.correo = correo;
+		this.proveedor = proveedor;
+		this.id_cli = id_cli;
+	}
+
+	public int getId_cli_cor() {
+		return id_cli_cor;
+	}
+
+	public void setId_cli_cor(int id_cli_cor) {
+		this.id_cli_cor = id_cli_cor;
+	}
+
+	public String getCorreo() {
+		return correo;
+	}
+
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+
+	public String getProveedor() {
+		return proveedor;
+	}
+
+	public void setProveedor(String proveedor) {
+		this.proveedor = proveedor;
+	}
+
+	public int getId_cli() {
+		return id_cli;
+	}
+
+	public void setId_cli(int id_cli) {
+		this.id_cli = id_cli;
+	}
+	
+	
+}
