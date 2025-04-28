90f725ca0625e8f233c2caf6cd7b3796669cd527
annkafake <lainfhj@proton.me>
2025-04-28 14:32:02 -0500

    Initialization and update logic and interface of publicidad

--- src/interfaces/IPublicidad.java
+++ src/interfaces/IPublicidad.java
@@ -6,9 +6,5 @@ import java.util.List;
 import entidades.Publicidad;
 
 public interface IPublicidad {
-	void guardar(Publicidad publicidad) throws SQLException;
-	void modificar(Publicidad publicidad) throws SQLException;
-	void borrar(Publicidad publicidad) throws SQLException;
-	
 	List<Publicidad> cargar() throws SQLException;
 }
