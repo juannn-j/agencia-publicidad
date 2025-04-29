6f25e888be54e6c17c217abda5858155bbfb6406
annkafake <lainfhj@proton.me>
2025-04-29 09:55:27 -0500

    test other server

--- src/datos/DB.java
+++ src/datos/DB.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DB {
-	private String url = "jdbc:postgresql://localhost/agenpub";
+	private String url = "jdbc:postgresql://localhost/agenpub2";
 	private String user = "postgres";
 	private String password = "123";
 	
